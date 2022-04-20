41aae25c1d29444c27855bc1ed3cfa9145bf6f67
Darweille <wimitsuhide@gmail.com>
2022-04-20 23:48:24 +0800

    Add files via upload

---
+++ convert.js
@@ -0,0 +1,21 @@
+﻿function BMI()
+{
+	var Height = Number(document.getElementById("inputHeight").value)/100;
+	var Weight = Number(document.getElementById("inputWeight").value);
+	var BMI = 0;
+	
+	if (Weight > 0 && Height > 0)
+	{
+		var BMI = Weight/(Height**2);
+	}
+	else
+	{
+		document.getElementById("inputBMI").value = "";
+	}
+
+		
+	if (BMI > 0)
+	{
+		document.getElementById("inputBMI").value = BMI.toFixed(2);
+	}
+}
